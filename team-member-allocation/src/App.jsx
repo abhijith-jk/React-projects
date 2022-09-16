67e878de7aaa12ac6362d04226b9851823bff814
Abhijith <abhijithjnk2001@gmail.com>
2022-09-16 20:54:59 +0530

    Installed bootstrap and added employees.jsx

---
+++ team-member-allocation/src/App.jsx
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Employees from './Employees';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <Employees/>
+      <Footer/>
+    </div>
+    
+  );
+}
+
+export default App;
