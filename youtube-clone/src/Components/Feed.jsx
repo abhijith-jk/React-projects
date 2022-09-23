0ec26b066fb5c81ed851a50f4e39aba2a937240e
Abhijith <abhijithjnk2001@gmail.com>
2022-09-23 18:29:44 +0530

    add: Components, API options object

--- youtube-clone/src/Components/Feed.jsx
+++ youtube-clone/src/Components/Feed.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import {useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Videos, SideBar } from './';
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx = {{ flexDirection : {sx:'column', md:'row'} }}>
+      <Box sx = {{ height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d' , px:{sx: 0, md: 2} }}>
+        <SideBar/>
+
+        <Typography className='copyright' variant='body2' sx = {{ mt: 1.5 , color: '#fff' }}>Copyright 2022 &#169;<br /><small sx = {{justifyContent : 'center'}}>Credit: JSM MEDIA</small></Typography>
+      </Box>
+
+      <Box p = {2} sx = {{ overflowY : 'auto', height :'90vh', flex: 2 }}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx = {{ color : 'white'}}> 
+          New <span style={{color : '#F31503'}}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
   )
 }
 
