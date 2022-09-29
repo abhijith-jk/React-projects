2ffa5fefb0f8978c6ea1b7ae15f354effd455b69
Abhijith <abhijithjnk2001@gmail.com>
2022-09-29 19:24:21 +0530

    Fully Functional

--- lyriks-spotify-clone/src/components/TopPlay.jsx
+++ lyriks-spotify-clone/src/components/TopPlay.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode } from "swiper";
+/* eslint-disable import/no-unresolved */
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
 
-import PlayPause from "./PlayPause";
-import { playPause, setActiveSong } from "../redux/features/playerSlice";
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
